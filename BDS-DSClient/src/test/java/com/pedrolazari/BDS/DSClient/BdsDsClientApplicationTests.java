5b9ed591555ee71f1c28bf6cab19f707003954d9
Pedro Lazari <67986647+PedroLazari233@users.noreply.github.com>
2022-08-17 18:04:12 -0300

    Adding dependencies using Spring Initializr

---
+++ BDS-DSClient/src/test/java/com/pedrolazari/BDS/DSClient/BdsDsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrolazari.BDS.DSClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdsDsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
